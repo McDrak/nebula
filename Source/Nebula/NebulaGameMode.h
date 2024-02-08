148975307f60326c6dcd11c1c4b60f1c122b0376
Alejandro Guayara <c_guayara@javeriana.edu.co>
2024-02-07 22:50:03 -0500

    Base Project Setup

---
+++ Source/Nebula/NebulaGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NebulaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANebulaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANebulaGameMode();
+};
+
+
+
